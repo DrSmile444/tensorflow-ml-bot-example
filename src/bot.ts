895430ff9d2901293ad4eb53a09a20a8bf9d11df
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2022-08-28 10:09:24 +0300

    style: reformat code

--- src/bot.ts
+++ src/bot.ts
@@ -1,28 +1,32 @@
 import * as process from 'node:process';
-import { Bot } from "grammy";
+import { Bot } from 'grammy';
+import { UserFromGetMe } from 'grammy/out/types';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Create an instance of the `Bot` class and pass your authentication token to it.
-const bot = new Bot(process.env['BOT_TOKEN']!); // <-- put your authentication token between the ""
+(async () => {
+  // Create an instance of the `Bot` class and pass your authentication token to it.
+  const bot = new Bot(process.env.BOT_TOKEN!); // <-- put your authentication token between the ""
 
-// You can now register listeners on your bot object `bot`.
-// grammY will call the listeners when users send messages to your bot.
+  // You can now register listeners on your bot object `bot`.
+  // grammY will call the listeners when users send messages to your bot.
 
-// Handle the /start command.
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-// Handle other messages.
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+  // Handle the /start command.
+  bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
+  // Handle other messages.
+  bot.on('message', (ctx) => ctx.reply('Got another message!'));
 
-// Now that you specified how to handle messages, you can start your bot.
-// This will connect to the Telegram servers and wait for messages.
+  // Now that you specified how to handle messages, you can start your bot.
+  // This will connect to the Telegram servers and wait for messages.
 
-// Start the bot.
-bot.start({
+  // Start the bot.
+  await bot.start({
     onStart: () => {
-        // @ts-ignore
-        console.info(`Bot @${bot.me.username} started!`, new Date().toString());
-    }
-});
-
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const botInfo = bot.me as UserFromGetMe;
+      console.info(`Bot @${botInfo.username} started!`, new Date().toString());
+    },
+  });
+})().catch(console.error);
