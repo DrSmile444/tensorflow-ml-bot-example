8b06fde814ee738279b062d2e63d637b8884134c
Dmytro Vakulenko <dmytro.vakulenko@masterofcode.com>
2023-01-31 20:01:12 +0200

    feat: integrate tensor with bot

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,16 @@
 import * as process from 'node:process';
 import dotenv from 'dotenv';
 import { Bot } from 'grammy';
-import { UserFromGetMe } from 'grammy/out/types';
+import type { UserFromGetMe } from 'grammy/out/types';
+
+import { initSwindlersTensorService } from './services';
 
 dotenv.config();
 
-await (async () => {
+// async iife
+(async () => {
+  const { swindlersTensorService } = await initSwindlersTensorService();
+
   // Create an instance of the `Bot` class and pass your authentication token to it.
   const bot = new Bot(process.env.BOT_TOKEN!); // <-- put your authentication token between the ""
 
@@ -15,7 +20,14 @@ await (async () => {
   // Handle the /start command.
   bot.command('start', (context) => context.reply('Welcome! Up and running.'));
   // Handle other messages.
-  bot.on('message', (context) => context.reply('Got another message!'));
+  bot.on('message', async (context) => {
+    const predictResult = await swindlersTensorService.predict(context.msg.text || '');
+
+    if (predictResult.isSpam) {
+      await context.deleteMessage();
+      await context.reply('Ваше повідомлення видалено по причині спам');
+    }
+  });
 
   // Now that you specified how to handle messages, you can start your bot.
   // This will connect to the Telegram servers and wait for messages.
@@ -29,4 +41,6 @@ await (async () => {
       console.info(`Bot @${botInfo.username} started!`, new Date().toString());
     },
   });
-})();
+})().catch(() => {
+  console.error('Bot has been stopped!');
+});
